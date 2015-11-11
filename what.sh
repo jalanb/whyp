37487620d6cc8e2c50e5ecf90a549cafc006e969
J Alan Brogan <github@al-got-rhythm.net>
2015-11-11 22:05:29 +0000

    New aliases for source_what

--- what.sh
+++ what.sh
@@ -130,6 +130,8 @@ source_what () {
     fi
     source "$@"
 }
+alias .=source_what
+alias require=source_what
 
 # Methods starting with underscores are intended for use in this file only
 #   (a convention borrowed from Python)
