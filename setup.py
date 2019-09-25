03f1456cc86b10999f8d7c0361a00de0860680cb
Administrator <gitlab@wwts.com>
2019-09-25 16:55:47 +0100

    Bring setup up to date

--- setup.py
+++ setup.py
@@ -4,26 +4,38 @@ from distutils.core import  setup
 
 import whyp
 
+description=whyp.__doc__
+
 setup(
-    name="whyp",
+    name=whyp.__name__,
     version=whyp.__version__,
-    description=whyp.__doc__.splitlines()[0],
-    url="https://github.com/jalanb/whyp",
-    requires=["argparse"],
-    packages=["whyp"],
-    scripts=["bin/whyp"],
+    description=description.splitlines()[0],
+    long_description=description,
+    url='https://github.com/jalanb/%s' % whyp.__name__,
+    packages=['whyp'],
+    download_url='https://github.com/jalanb/%s/tarball/v%s' % (
+        whyp.__name__, whyp.__version__),
+    license='MIT License',
+    author='jalanb',
+    author_email='github@al-got-rhythm.net',
     platforms='any',
     classifiers=[
+        'Programming Language :: Python :: 3.7',
         'Development Status :: 2 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
         'Topic :: Software Development',
         'Topic :: Utilities'
     ],
-    author="jalanb",
-    author_email='github@al-got-rhythm.net',
+    install_requires=[
+        'pprintpp',
+        'pysyte',
+        'pyyaml',
+        'requests',
+        'six',
+    ],
+    scripts=['bin/whyp'],
 )
 
